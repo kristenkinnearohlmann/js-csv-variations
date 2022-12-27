f3f748b57644381789691fecfba59c219214c990
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-12-26 19:01:46 -0600

    Add nodemon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
