36b17a737e393938739c9dd054858182c09c2453
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2022-12-31 18:12:44 -0600

    Comment first trial, rename id for second trial

--- src/util.js
+++ src/util.js
@@ -1,39 +1,41 @@
-const btnSelect = document.getElementById("btnSelect");
-const btnInput = document.getElementById("btnInput");
-const fileItem = document.getElementById("fileItem");
+// const btnSelect = document.getElementById("btnSelect");
+// const btnInput = document.getElementById("btnInput");
+// const fileItem = document.getElementById("fileItem");
 
-btnSelect.addEventListener(
-  "click",
-  (event) => {
-    if (fileItem) {
-      fileItem.click();
-    }
-  },
-  false
-);
+// btnSelect.addEventListener(
+//   "click",
+//   (event) => {
+//     if (fileItem) {
+//       fileItem.click();
+//     }
+//   },
+//   false
+// );
+
+// fileItem.addEventListener("change", () => {
+//   console.log("The thing changed");
+//   console.log(fileItem.files[0].name);
+//   document.getElementById("fileNameDisplay").value = fileItem.files[0].name;
+// });
+
+// btnInput.addEventListener("click", (event) => {
+//   const selectedFile = getFileSelection();
+
+//   selectedFile ? handleFileSelected(selectedFile) : handleNoFileSelected();
+// });
+
+// const getFileSelection = () => {
+//   //   return document.getElementById("fileItem").files[0];
+//   return fileItem.files[0];
+// };
+
+// const handleFileSelected = (selectedFile) => {
+//   console.log(selectedFile);
+// };
+
+// const handleNoFileSelected = () => {
+//   console.log("No file selected");
+// };
+
 // TODO: Add code from here?
 // https://javascript.info/file
-fileItem.addEventListener("change", () => {
-  console.log("The thing changed");
-  console.log(fileItem.files[0].name);
-  document.getElementById("fileNameDisplay").value = fileItem.files[0].name;
-});
-
-btnInput.addEventListener("click", (event) => {
-  const selectedFile = getFileSelection();
-
-  selectedFile ? handleFileSelected(selectedFile) : handleNoFileSelected();
-});
-
-const getFileSelection = () => {
-  //   return document.getElementById("fileItem").files[0];
-  return fileItem.files[0];
-};
-
-const handleFileSelected = (selectedFile) => {
-  console.log(selectedFile);
-};
-
-const handleNoFileSelected = () => {
-  console.log("No file selected");
-};
