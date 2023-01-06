8cde3dbb4258c62324d4321e0a5f1154c24daedb
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2023-01-06 13:31:14 -0600

    Iterate on example from MDN documentation

--- src/util.js
+++ src/util.js
@@ -1,78 +1,102 @@
+const fileInput = document.getElementById("choose-file");
+
+const readFileInput = (callback) => {
+  console.log("Reached readFileInput");
+  const file = fileInput.files.item(0);
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    callback(reader.result);
+  };
+
+  reader.readAsText(file);
+};
+
+const handleFileInput = (result) => {
+  console.log("Reached handleFileInput");
+  console.log(result);
+};
+
+fileInput.addEventListener("change", (event) => {
+  console.log("Reached change event for file input");
+  readFileInput(handleFileInput);
+});
+
 // TODO: Add code from here?
 // https://javascript.info/file
 
-let selectedFile;
+// let selectedFile;
 
-document.getElementById("choose-file").addEventListener("change", (event) => {
-  let reader = new FileReader();
+// document.getElementById("choose-file").addEventListener("change", (event) => {
+//   let reader = new FileReader();
 
-  selectedFile = document.getElementById("choose-file").files[0];
-  reader.readAsText(selectedFile);
+//   selectedFile = document.getElementById("choose-file").files[0];
+//   reader.readAsText(selectedFile);
 
-  reader.onload = () => {
-    console.log(reader.result);
-  };
+//   reader.onload = () => {
+//     console.log(reader.result);
+//   };
 
-  reader.onerror = () => {
-    console.log(reader.error);
-  };
-});
+//   reader.onerror = () => {
+//     console.log(reader.error);
+//   };
+// });
 
-document.getElementById("input-file").addEventListener("click", (event) => {
-  const data = processInputFile(selectedFile);
-  // postData(data);
-});
+// document.getElementById("input-file").addEventListener("click", (event) => {
+//   const data = processInputFile(selectedFile);
+//   // postData(data);
+// });
 
-const processInputFile = (fileInput) => {
-  console.log("What's the file input", fileInput);
-  let data = { env: "", records: [] };
-  const reader = new FileReader();
-  reader.readAsText(fileInput);
+// const processInputFile = (fileInput) => {
+//   console.log("What's the file input", fileInput);
+//   let data = { env: "", records: [] };
+//   const reader = new FileReader();
+//   reader.readAsText(fileInput);
 
-  // TODO: How to defeat this asynchronous process??
-  // Potential? https://developer.mozilla.org/en-US/docs/Web/API/FileReader
-  reader.onload = () => {
-    const result = reader.result;
-    const dataArray = result.split("\r\n");
-    const columnLabels = dataArray.shift().split("|");
-    const columnHeaders = {};
-    const records = [];
+//   // TODO: How to defeat this asynchronous process??
+//   // Potential? https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+//   reader.onload = () => {
+//     const result = reader.result;
+//     const dataArray = result.split("\r\n");
+//     const columnLabels = dataArray.shift().split("|");
+//     const columnHeaders = {};
+//     const records = [];
 
-    columnLabels.forEach((label, index) => {
-      columnHeaders[index] = label;
-    });
+//     columnLabels.forEach((label, index) => {
+//       columnHeaders[index] = label;
+//     });
 
-    do {
-      let record = {};
-      let rawRecord = dataArray.pop();
+//     do {
+//       let record = {};
+//       let rawRecord = dataArray.pop();
 
-      rawRecord.split("|").forEach((item, index) => {
-        record[columnLabels[index]] = item === "[NULL]" ? "" : item;
-      });
-      records.push(record);
-    } while (dataArray.length > 0);
+//       rawRecord.split("|").forEach((item, index) => {
+//         record[columnLabels[index]] = item === "[NULL]" ? "" : item;
+//       });
+//       records.push(record);
+//     } while (dataArray.length > 0);
 
-    console.log(records);
-    data = { ...columnHeaders, records: records };
-  };
+//     console.log(records);
+//     data = { ...columnHeaders, records: records };
+//   };
 
-  console.log(data);
+//   console.log(data);
 
-  return {
-    env: "Dev",
-    records: [
-      { name: "Mario", type: "cow" },
-      { name: "Rucola", type: "tabby" },
-    ],
-  };
-};
+//   return {
+//     env: "Dev",
+//     records: [
+//       { name: "Mario", type: "cow" },
+//       { name: "Rucola", type: "tabby" },
+//     ],
+//   };
+// };
 
-const postData = (data) => {
-  fetch("http://localhost:5001/file-import/", {
-    method: "POST",
-    mode: "no-cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
-  // TODO: Add handling of resolved/rejected promise
-};
+// const postData = (data) => {
+//   fetch("http://localhost:5001/file-import/", {
+//     method: "POST",
+//     mode: "no-cors",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify(data),
+//   });
+//   // TODO: Add handling of resolved/rejected promise
+// };
